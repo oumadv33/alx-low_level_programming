9e42014f523374c013471475b387f42e3f5d885d
oumayma <alomk@gmail.com>
2023-03-16 10:38:59 -0700

    4-puts.c git push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
 *main - A program that prints a line with puts function
-*return : 0 (Succes)
+*Return : 0 (Succes)
 */
 int main(void) {
 	puts("\"programing is like building a multilingual puzzle");
