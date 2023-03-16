e2e134c051a5559c5b1db309882de1fe6ec56d0e
oumayma <alomk@gmail.com>
2023-03-16 13:09:49 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints out a string to sdtout.
+ * main - A program that print a line using the printf function
  * Return: 0(Success)
  */
-int main(void) {
-	pritf("with proper grammar, but the outcome is a piece of art,\n");
+int main(void)
+{
+	pritf("with proper grammar, but the outcome is a piece of art,\n")
 	return (0);
 }
