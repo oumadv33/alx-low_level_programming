3595093da6cdfca22c1e8da96ec82ff9d80c719b
oumayma <alomk@gmail.com>
2023-03-16 11:10:18 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - prints out a string to sdtout.
+ * Return: 0(Success)
+ */
+int main(void) {
+	pritf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
