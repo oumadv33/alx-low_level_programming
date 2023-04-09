7d4bede99d743296ed66bea4568ab4f139b4254c
oumayma <alomk@gmail.com>
2023-04-08 19:55:57 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,19 +9,19 @@
 
 int main(int argc, char *argv[])
 {
-int x, y, mul;
+	int x, y, mul;
 
-if (argc != 3)
-{
-printf("Error\n");
-return (1);
-}
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-x = atoi(argv[1]);
-y = atoi(argv[2]);
-mul = x * y;
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	mul = x * y;
 
-printf("%d\n", mul);
+	printf("%d\n", mul);
 
-return (0);
+	return (0);
 }
