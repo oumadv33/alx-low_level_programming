2a70b7cd4c090a5d164edf9040b0731aeb04ed54
oumayma <alomk@gmail.com>
2023-04-09 16:23:06 -0700

    limby

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,6 @@
-#ifndef MYHEADER_H
-#define MYHEADER_H
-
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,5 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MYHEADER_H */
-
+#endif
